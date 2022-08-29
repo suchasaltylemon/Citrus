7c87fd8931ea6c6d050299dd2ec92a2561059522
Sam <46686300+suchasaltylemon@users.noreply.github.com>
2022-08-29 22:13:32 +0100

    Changed port to default
    TODO:
     * Implement pygame event loop and work on components + API

--- example/server_runtime.py
+++ example/server_runtime.py
@@ -4,6 +4,6 @@ add_path("server/services/")
 add_path("server/components/")
 add_path("shared/components/")
 
-start("0.0.0.0", port=7777, do_debug=True, log_path="./server.log", log_to_stream=True)
+start("0.0.0.0", do_debug=True, log_path="./server.log")
 
 # TODO: Implement pygame runtime
